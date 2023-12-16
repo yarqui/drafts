3045835e8553fc5e471d84bea41e0ffef140df62
yarqui <y.pelykh@gmail.com>
2023-12-16 10:17:08 -0500

    add playwright

--- main.js
+++
@@ -1,8 +0,0 @@
-const first = { propA: 5, propB: 10, propC: 50 };
-const second = { propC: 15 };
-
-const third = { propB: 20, ...first, ...second };
-console.log("third", third); // { propA: 5, propB: 10, propC: 15 }
-
-const fourth = { ...first, ...second, propB: 20 };
-console.log("fourth:", fourth); // { propA: 5, propB: 20, propC: 15 }
